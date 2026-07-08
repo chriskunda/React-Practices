6f8efec59bc88e648dfd6b2bfd3a76340bee31ef
chriskunda <christianira2003@gmail.com>
2026-07-08 13:50:04 +0200

    ...

--- learning-react/src/App.jsx
+++ learning-react/src/App.jsx
@@ -1,17 +1,19 @@
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
-
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <h1>Greetings</h1>
+      <Hello name='George' />
+      <Hello name='Daisy' />
     </div>
   )
 }
 
 export default App
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}</p>
+    </div>
+  )
+}
