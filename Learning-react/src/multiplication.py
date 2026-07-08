084c5058c081d20ea3a4220e979740c90069dd44
chriskunda <christianira2003@gmail.com>
2026-07-08 13:24:44 +0200

    starting learning React from scratch

--- Learning-react/src/multiplication.py
+++
@@ -1,6 +0,0 @@
-data = [15, 7, 30, 45, 8, 60, 3, 20]
-
-for index, value in enumerate(data):
-
-    if value % 5 == 0 and value % 3 == 0:
-        print("Index:", index, "Value:", value)
--- README.md
+++
@@ -1,2 +0,0 @@
-# React-Practices
-Learning React
