40016b35ab86304cb9343cd56689e27b7129553e
chriskunda <christianira2003@gmail.com>
2026-07-10 18:33:21 +0200

    ...

--- learning-react/src/App.jsx
+++ learning-react/src/App.jsx
@@ -3,16 +3,14 @@ import { useState } from 'react'
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-  const handleClick = () => {
-    setCounter(counter + 1)
-    console.log('clicked')
-  }
-
   return (
     <div>
       <div>{counter}</div>
-      <button onClick={handleClick}>
+      <button onClick={() => setCounter(counter + 1)}>
         plus
+      </button>
+      <button onClick={() => setCounter(0)}> 
+        zero
       </button>
     </div>
   )
