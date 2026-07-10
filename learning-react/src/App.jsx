45de358848771ad2606816d3bd49c4596b08726c
chriskunda <christianira2003@gmail.com>
2026-07-10 19:15:26 +0200

    ...

--- learning-react/src/App.jsx
+++ learning-react/src/App.jsx
@@ -1,5 +1,19 @@
 import { useState } from 'react'
 
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
 const App = () => {
   const [counter, setCounter] = useState(0)
   console.log('rendering with counter value', counter)
