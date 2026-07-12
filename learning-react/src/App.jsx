cb1fe0645b34135d5b80a5d3921f22de7cff79ae
chriskunda <christianira2003@gmail.com>
2026-07-12 12:39:34 +0200

    ,,,

--- learning-react/src/App.jsx
+++ learning-react/src/App.jsx
@@ -23,27 +23,12 @@ const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
+  const [value, setValue] = useState(10)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1
-    setLeft(updatedLeft)
-    setTotal(updatedLeft + right) 
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
   }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    const updatedRight = right +1
-    setRight(updatedRight)
-    setTotal(updatedRight + left)
-  }
-
-  console.log(allClicks)
-  console.log(total)
 
 
   return (
