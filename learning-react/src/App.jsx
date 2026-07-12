a9cda060331aaed48f0d927a22c38e51ebcf0ad5
chriskunda <christianira2003@gmail.com>
2026-07-12 14:23:51 +0200

    ..

--- learning-react/src/App.jsx
+++ learning-react/src/App.jsx
@@ -1,8 +1,16 @@
+const App = (props) => {
+  const { notes } = props
 
-const App = () => {
-  
-
-  return ();
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li>
+      </ul>
+    </div>
+  )
 }
 
 export default App
