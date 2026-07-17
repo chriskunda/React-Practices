21df2d75e8f607faef6d9a3013e6a33f61c71d50
chriskunda <christianira2003@gmail.com>
2026-07-17 15:48:15 +0200

    ...

--- learning-react/src/main.jsx
+++ learning-react/src/main.jsx
@@ -2,10 +2,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import axios from 'axios'
 
-axios.get('http://localhost:3001/notes').then(response => {
-  const notes = response.data
-  console.log(notes)
-})
+// axios.get('http://localhost:3001/notes').then(response => {
+//   const notes = response.data
+//   console.log(notes)
+// })
 
 
 const notes = [
@@ -26,6 +26,7 @@ const notes = [
   }
 ]
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App notes={notes} />
-)
+axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data
+  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+})
