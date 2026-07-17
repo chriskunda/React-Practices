2a4859e17f9f517cdd305adb0f55830392d1ec7a
chriskunda <christianira2003@gmail.com>
2026-07-17 22:39:19 +0200

    ...

--- learning-react/src/App.jsx
+++ learning-react/src/App.jsx
@@ -33,6 +33,10 @@ const App = () => {
     })
   }
 
+  const toggleImportanceOf = (id) => {
+    console.log(`importance of ${id} needs to be toggled`)
+  }
+
   const handleNoteChange = (event) => {
     setNewNote(event.target.value)
   }
@@ -49,7 +53,7 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
         ))}
       </ul>
       <form onSubmit={addNote}>
