05c8e27644dab0341a31f5b2a8cdccf2bf285511
chriskunda <christianira2003@gmail.com>
2026-07-17 22:50:14 +0200

    ...

--- learning-react/src/App.jsx
+++ learning-react/src/App.jsx
@@ -40,9 +40,15 @@ const App = () => {
   const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
   
-  const toggleImportanceOf = (id) => {
-    console.log(`importance of ${id} needs to be toggled`)
-  }
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(note => note.id === id ? response.data : note))
+    })
+}
 
 
   return (
