87d89c06bd53071832a3d6be48d54d3cd792cc30
chriskunda <christianira2003@gmail.com>
2026-07-18 22:16:11 +0200

    ...

--- learning-react/src/App.jsx
+++ learning-react/src/App.jsx
@@ -8,6 +8,7 @@ const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(() => {
     noteService
