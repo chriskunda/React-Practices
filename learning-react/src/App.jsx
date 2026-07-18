124d43a65a48bfd37ff1b8f746b214410590d1da
chriskunda <christianira2003@gmail.com>
2026-07-18 22:24:12 +0200

    ...

--- learning-react/src/App.jsx
+++ learning-react/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import Footer from './components/Footer'
 import Note from './components/Notes'
 import noteService from './services/notes'
 import Notification from './components/Notification'
