a058c28077a35f12aa4f82ed5ef377f62fe1e9b2
chriskunda <christianira2003@gmail.com>
2026-07-24 17:59:42 +0200

    ....

--- learning-react/backend/index.js
+++ learning-react/backend/index.js
@@ -48,24 +48,6 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-// app.post('/api/notes', (request, response) => {
-//   const note = request.body
-//   console.log(note)
-//   response.json(note)
-// })
-
-// app.post('/api/notes', (request, response) => {
-//   const maxId = notes.length > 0
-//     ? Math.max(...notes.map(n => Number(n.id))) 
-//     : 0
-
-//   const note = request.body
-//   note.id = String(maxId + 1)
-
-//   notes = notes.concat(note)
-
-//   response.json(note)
-// })
 
 const generateId = () => {
   const maxId = notes.length > 0
