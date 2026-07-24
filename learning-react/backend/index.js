b4d5c86cf8761a681f8d73b5ed0c53bd92e7f8b0
chriskunda <christianira2003@gmail.com>
2026-07-24 15:24:04 +0200

    if condition to handle the error

--- learning-react/backend/index.js
+++ learning-react/backend/index.js
@@ -31,7 +31,11 @@ app.get('/api/notes/:id', (request, response) => {
   const id = request.params.id
   const note = notes.find(note => note.id === id)
 
-  
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
 const PORT = 3001
