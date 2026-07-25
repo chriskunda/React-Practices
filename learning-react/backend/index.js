8f08e08668e157bca7edaf54349ea10c832120cf
chriskunda <christianira2003@gmail.com>
2026-07-25 17:01:53 +0200

    ...

--- learning-react/backend/index.js
+++ learning-react/backend/index.js
@@ -90,3 +90,9 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(requestLogger)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
