9d28e760ddb6126b55ef51f5a994646ec2ed81f0
chriskunda <christianira2003@gmail.com>
2026-07-26 14:23:37 +0200

    ...

--- learning-react/backend/index.js
+++ learning-react/backend/index.js
@@ -93,7 +93,7 @@ const cors = require('cors')
 
 app.use(cors())
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
