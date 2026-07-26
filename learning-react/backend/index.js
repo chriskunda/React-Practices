37832154a8670cf17579661af5208e2744693190
chriskunda <christianira2003@gmail.com>
2026-07-26 15:21:13 +0200

    ...

--- learning-react/backend/index.js
+++ learning-react/backend/index.js
@@ -30,9 +30,9 @@ const requestLogger = (request, response, next) => {
 app.use(express.json())
 app.use(requestLogger)
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+// app.get('/', (request, response) => {
+//   response.send('<h1>Hello World!</h1>')
+// })
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
