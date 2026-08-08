6d296431519e4cbb2573d5e5803795928309e3fe
chriskunda <christianira2003@gmail.com>
2026-08-08 13:36:43 +0200

    ..

--- learning-react/backend/index.js
+++ learning-react/backend/index.js
@@ -56,24 +56,10 @@ const generateId = () => {
   return String(maxId + 1)
 }
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing',
-    })
-  }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    id: generateId(),
-  }
-
-  notes = notes.concat(note)
-
-  response.json(note)
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
